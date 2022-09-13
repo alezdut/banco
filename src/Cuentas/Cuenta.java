3dd3c2b8a55192ce8d83133a8dad1a3f9f8afc2a
Tu nombre <tototemporelli12@gmail.com>
2022-09-13 17:55:48 -0300

    Intente crear la clase cuenta y pushearla al proyecto

---
+++ src/Cuentas/Cuenta.java
@@ -0,0 +1,26 @@
+package Cuentas;
+
+public abstract class Cuenta {
+    private int numCuenta;
+    //private Usuario titular;
+    float saldo;
+    String divisa;      //o enum
+
+    public Cuenta(int numCuenta, float saldo, String divisa) {
+        this.numCuenta = numCuenta;
+        this.saldo = saldo;
+        this.divisa = divisa;
+    }
+
+    public int getNumCuenta() {return numCuenta;}
+
+    public void setNumCuenta(int numCuenta) {this.numCuenta = numCuenta;}
+
+    public float getSaldo() {return saldo;}
+
+    public void setSaldo(float saldo) {this.saldo = saldo;}
+
+    public String getDivisa() {return divisa;}
+
+    public void setDivisa(String divisa) {this.divisa = divisa;}
+}
